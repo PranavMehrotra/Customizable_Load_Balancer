749fc1d749e783d173c36fa5098253ac1e64f906
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2024-01-14 00:22:19 +0530

    Corrected Logic of Collision Handling 1. Changed the data structures a bit to optimise and increase efficiency 2. Implemented Remove Server 3. Corrected Linear Probing logic for addition and deletions 4. Tested the code with sample runs

--- consistent_hashing/consistent_hashing.py
+++ consistent_hashing/consistent_hashing.py
@@ -1,7 +1,5 @@
-import os
-import sys
-import threading
 import bisect
+import time
 import numpy as np
 
 from RWLock import RWLock
@@ -16,47 +14,77 @@ class ConsistentHashing:
         self.num_slots = num_slots
         self.lock = RWLock()
         self.hash_map = []
+        self.hash_array = np.zeros(self.num_slots)
         self.id_to_hostname = {}
+        self.hostname_to_id = {}
         self.num_virtual_servers = 0
-        self.next_server_id = 0
+        self.next_server_id = 1
         self.init_hash_map(server_hostnames)
 
     
     def init_hash_map(self, server_hostnames: list):
         self.lock.acquire_writer()
-        self.hash_map = []
         for i in range(self.num_servers):
-            self.id_to_hostname[i] = server_hostnames[i]
+            self.id_to_hostname[self.next_server_id] = server_hostnames[i]
+            self.hostname_to_id[server_hostnames[i]] = self.next_server_id
             for j in range(self.num_replicas):
-                self.hash_map.append((self.server_hash_func(i, j), i))
-        self.hash_map.sort(key=lambda x: x[0])
+                replica_hash = self.server_hash_func(self.next_server_id, j)
+                replica_hash = self.linear_probing(replica_hash)
+                self.hash_map.append(replica_hash)
+                self.hash_array[replica_hash] = self.next_server_id
+            self.next_server_id += 1
+        self.hash_map.sort()
         self.num_virtual_servers = len(self.hash_map)
-        self.next_server_id = self.num_servers
         self.lock.release_writer()
+        # self.__unique_checker()
 
     def server_hash_func(self, server_id, replica_id):
         hash = (server_id*server_id) % self.num_slots
         hash += (replica_id*replica_id) % self.num_slots
         hash += (2*replica_id + 25) % self.num_slots
+        print(f"Server: {server_id}, Replica: {replica_id}, Hash: {hash}")
         return hash % self.num_slots
     
     def request_hash_func(self, request_id):
         hash = (request_id*request_id) % self.num_slots
         hash += (2*request_id + 17) % self.num_slots
+        print(f"Request: {request_id}, Hash: {hash}")
         return hash % self.num_slots
     
     def get_server(self, request_id):
         self.lock.acquire_reader()
-        server_id = self.hash_map[bisect.bisect_left(self.hash_map, (self.request_hash_func(request_id),))][1]
+        server_id = self.hash_array[self.hash_map[bisect.bisect_left(self.hash_map, self.request_hash_func(request_id)) % self.num_virtual_servers]]
+        server = self.id_to_hostname[server_id]
         self.lock.release_reader()
-        return self.id_to_hostname[server_id]
+        return server
     
-    def linear_probing(self, idx):
-        assert self.hash_map[(idx+1)%self.num_virtual_servers][0] != self.hash_map[idx][0]
-        while self.hash_map[(idx+1)%self.num_virtual_servers][0] == self.hash_map[idx][0] + 1:
-            idx = (idx+1)%self.num_virtual_servers
-        return idx+1
+    def linear_probing(self, replica_hash):
+        i = replica_hash
+        if self.hash_array[i] != 0:
+            print(f"Collision while adding at hash {replica_hash}, performing linear probing")
+        while self.hash_array[i] != 0:
+            i = (i+1) % self.num_slots
+            if i == replica_hash:
+                # Should never happen
+                BufferError("No more slots available")
+                return
+        if i != replica_hash:
+            print(f"Linear probing completed at hash {i}")
+        return i
 
+    def linear_probing_delete(self, replica_hash, server_id):
+        i = replica_hash
+        if self.hash_array[i] != server_id:
+            print(f"Collision while removing at hash {replica_hash}, performing linear probing")
+        while self.hash_array[i] != server_id:
+            i = (i+1) % self.num_slots
+            if i == replica_hash:
+                # Should never happen
+                BufferError(f"Replica does not exist of server {server_id}")
+                return
+        if i != replica_hash:
+            print(f"Linear probing completed at hash {i}")
+        return i
 
     def add_server(self, server_hostname):
         if self.num_virtual_servers + self.num_replicas > self.num_slots:
@@ -66,14 +94,81 @@ class ConsistentHashing:
         server_id = self.next_server_id
         self.next_server_id += 1
         self.id_to_hostname[server_id] = server_hostname
+        self.hostname_to_id[server_hostname] = server_id
         for i in range(self.num_replicas):
             replica_hash = self.server_hash_func(server_id, i)
-            temp_idx = bisect.bisect_left(self.hash_map, (replica_hash,))
-            if self.hash_map[temp_idx][0] == replica_hash:
-                temp_idx = self.linear_probing(temp_idx)
-            if temp_idx == self.num_virtual_servers:
-                self.hash_map.append((replica_hash, server_id))
-            else:
-                self.hash_map.insert(temp_idx, (replica_hash, server_id))
+            replica_hash = self.linear_probing(replica_hash)
+            bisect.insort(self.hash_map, replica_hash)
+            self.hash_array[replica_hash] = server_id
             self.num_virtual_servers += 1
         self.lock.release_writer()
+        # self.__unique_checker()
+
+    def remove_server(self, server_hostname):
+        # Check if server exists
+        if server_hostname not in self.hostname_to_id:
+            print("Server does not exist")
+            return
+        self.lock.acquire_writer()
+        server_id = self.hostname_to_id[server_hostname]
+        del self.hostname_to_id[server_hostname]
+        del self.id_to_hostname[server_id]
+        for i in range(self.num_replicas):
+            replica_hash = self.server_hash_func(server_id, i)
+            replica_hash = self.linear_probing_delete(replica_hash, server_id)
+            self.hash_array[replica_hash] = 0
+            idx = bisect.bisect_left(self.hash_map, replica_hash)
+            if idx >= len(self.hash_map) or self.hash_map[idx] != replica_hash:
+                # Should never happen
+                # print(f"Replica {i} of server {server_id} does not exist")
+                self.lock.release_writer()
+                BufferError(f"Replica {i} of server {server_id} does not exist")
+            del self.hash_map[idx]
+            self.num_virtual_servers -= 1
+        self.lock.release_writer()
+        # self.__unique_checker()
+
+    def print_hash_map(self):
+        self.lock.acquire_reader()
+        print(f"Number of virtual servers: {self.num_virtual_servers}\n Hash map: ", end="")
+        print(self.hash_map)
+        # print(self.hash_array)
+        self.lock.release_reader()
+
+    def __unique_checker(self):
+        # Check if sorted list is unique
+        print("Checking uniqueness of hash map: ", end="")
+        self.lock.acquire_reader()
+        for i in range(len(self.hash_map)-1):
+            if self.hash_map[i] == self.hash_map[i+1]:
+                print("Not unique")
+                print(f"Duplicate: {self.hash_map[i]} at index {i} and {i+1}")
+                print(self.hash_map)
+                self.lock.release_reader()
+                exit()
+        self.lock.release_reader()
+        print("Unique")
+
+
+if __name__ == "__main__":
+    server_hostnames = ["server1", "server2", "server3"]
+    consistent_hashing = ConsistentHashing(server_hostnames)
+    consistent_hashing.print_hash_map()
+    # time.sleep(5)
+    consistent_hashing.add_server("server4")
+    consistent_hashing.print_hash_map()
+    # time.sleep(5)
+    consistent_hashing.remove_server("server2")
+    consistent_hashing.print_hash_map()
+    # time.sleep(5)
+    # consistent_hashing.add_server("server5")
+    # consistent_hashing.print_hash_map()
+    # consistent_hashing.remove_server("server3")
+    # consistent_hashing.print_hash_map()
+    # consistent_hashing.add_server("server6")
+    # consistent_hashing.print_hash_map()
+    # consistent_hashing.remove_server("server1")
+    # consistent_hashing.print_hash_map()
+    print(consistent_hashing.get_server(1))
+    print(consistent_hashing.get_server(2))
+    print(consistent_hashing.get_server(5))
--- consistent_hashing/test.py
+++ consistent_hashing/test.py
@@ -1,12 +1,17 @@
 import bisect
-
+import numpy as np
 # Create a sorted list
-nums = [(1,0), (2,0), (5,0), (7,0), (9,1), (10, 1)]
+# nums = [(1,0), (2,0), (5,0), (7,0), (9,1), (10, 1)]
 
 # Find the index where to insert 2
-index = bisect.bisect_left(nums, (20,))
+# index = bisect.bisect_left(nums, (20,))
 
-print(nums)
+# nums.insert(index, (20, 1))
 
 # Print the index
-print(index)
+# print(index)
+
+a = np.zeros((5,2), dtype=int)
+a[1] = 1,2
+a[3] = 3,42
+print(a)
