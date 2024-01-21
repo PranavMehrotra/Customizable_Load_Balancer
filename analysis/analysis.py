9358070f80f083159761af214a3f94ee7b79c256
jena-shreyas <jenashreyas@gmail.com>
2024-01-21 22:44:13 +0530

    basic testing analysis

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -12,17 +12,17 @@ async def send_request(
         port: int = 5000, 
         path: str = "/home"):
     try:
-        async with session.get(f'http://{address}:{port}/{path}') as response:
+        async with session.get(f'http://{address}:{port}{path}') as response:
             print(f"Request ID: {request_id} | Status Code: {response.status} - Response: {await response.text()}")
     except Exception as e:
         print(f"Request ID: {request_id} | Error: An exception occurred during client request: {e}")
 
-async def send_requests(num_requests: int, address: str):
+async def send_requests(num_requests: int, address: str, port_no: int):
     try:
         async with aiohttp.ClientSession() as session:
             tasks = []
             for i in range(num_requests):
-                tasks.append(send_request(session, i+1, address))
+                tasks.append(send_request(session, i+1, address, port=port_no))
             await asyncio.gather(*tasks)
     except Exception as e:
         print(f"Error: An exception occurred while sending multiple client requests: {e}")
@@ -35,16 +35,14 @@ if __name__ == '__main__':
             print("Error: LB_ADDRESS not set!")
             exit()      # replace with default load balancer address (to be set)
 
+        port_no = 32769     # set to this for now, as LB not ready yet - finally 5000
+
         start = time.time()
         # Send 10000 requests to the load balancer
-        asyncio.run(send_requests(10000, lb_address))
+        asyncio.run(send_requests(10000, lb_address, port_no))
         end = time.time()
 
         print(f"Time taken to send 10000 requests: {end-start} seconds")
 
     except Exception as e:
         print(f"Error: An exception occurred in overall run: {e}")
-
-
-
-
