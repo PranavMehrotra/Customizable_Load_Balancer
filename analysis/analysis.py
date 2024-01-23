ffa7f4f6ee7dbd169de908962a7ecf22169b4c3e
jena-shreyas <jenashreyas@gmail.com>
2024-01-23 19:37:54 +0530

    barplot

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -2,8 +2,11 @@ import aiohttp
 import asyncio
 import time
 import os
+import matplotlib.pyplot as plt
+import json
 
 # write code to send 10000 async requests to the load balancer via GET requests
+frequency_map = {}
 
 async def send_request(
         session: aiohttp.ClientSession, 
@@ -11,9 +14,21 @@ async def send_request(
         address: str, 
         port: int = 5000, 
         path: str = "/home"):
+    
+    global frequency_map
     try:
         async with session.get(f'http://{address}:{port}{path}') as response:
-            print(f"Request ID: {request_id} | Status Code: {response.status} - Response: {await response.text()}")
+            resp = await response.text()
+            print(f"Request ID: {request_id} | Status Code: {response.status} - Response: {resp}")
+            resp = json.loads(resp)
+            
+            message = resp['message']
+            server_name = message.split(' ')[-1]
+            if server_name not in frequency_map:
+                frequency_map[server_name] = 1
+            else:
+                frequency_map[server_name] += 1
+
     except Exception as e:
         print(f"Request ID: {request_id} | Error: An exception occurred during client request: {e}")
 
@@ -27,21 +42,28 @@ async def send_requests(num_requests: int, address: str, port_no: int):
     except Exception as e:
         print(f"Error: An exception occurred while sending multiple client requests: {e}")
 
+def plot_bar_chart(frequency_map):
+    servers = list(frequency_map.keys())
+    frequencies = list(frequency_map.values())
+
+    plt.bar(servers, frequencies, color='blue')
+    plt.xlabel('Server Number')
+    plt.ylabel('Frequency')
+    plt.title('Frequency of Servers')
+    plt.show()
+
 if __name__ == '__main__':
     try:
         # Get the address of the load balancer
-        lb_address = os.environ.get("LB_ADDRESS")
-        if lb_address is None:
-            print("Error: LB_ADDRESS not set!")
-            exit()      # replace with default load balancer address (to be set)
-
-        port_no = 32769     # set to this for now, as LB not ready yet - finally 5000
+        lb_address = "127.0.0.1"
+        port_no = 5000     # set to this for now, as LB not ready yet - finally 5000
 
         start = time.time()
         # Send 10000 requests to the load balancer
         asyncio.run(send_requests(10000, lb_address, port_no))
         end = time.time()
 
+        plot_bar_chart(frequency_map)
         print(f"Time taken to send 10000 requests: {end-start} seconds")
 
     except Exception as e:
